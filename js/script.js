7e8f8d5194cf20d9d2b62b5b587aca66f422fff7
rubiii <me@rubiii.com>
2015-08-03 11:10:20 +0200

    use double quotes.

--- js/script.js
+++ js/script.js
@@ -1,37 +1,37 @@
 jQuery(function() {
-  var $popup = jQuery('#epages-popup-content');
+  var $popup = jQuery("#epages-popup-content");
 
-  jQuery('#insert-epages-button').click(epages_open_popup);
+  jQuery("#insert-epages-button").click(epages_open_popup);
 
-  jQuery('.media-modal-close', $popup).click(function() {
-    $popup.removeClass('open');
+  jQuery(".media-modal-close", $popup).click(function() {
+    $popup.removeClass("open");
     return false;
   });
 
   jQuery(document).keydown(function(e) {
-    if (e.keyCode == 27 && $popup.hasClass('open')) {
-      $popup.removeClass('open');
+    if (e.keyCode == 27 && $popup.hasClass("open")) {
+      $popup.removeClass("open");
       return false;
     }
   });
 
-  jQuery('.media-menu-item', $popup).click(function() {
-    jQuery('.media-menu .media-menu-item', $popup).removeClass('active');
-    jQuery(this).addClass('active');
+  jQuery(".media-menu-item", $popup).click(function() {
+    jQuery(".media-menu .media-menu-item", $popup).removeClass("active");
+    jQuery(this).addClass("active");
 
-    jQuery('.media-modal-content', $popup).attr('data-active-dialog', jQuery(this).attr('data-content'));
-    jQuery('.media-menu').removeClass('visible');
+    jQuery(".media-modal-content", $popup).attr("data-active-dialog", jQuery(this).attr("data-content"));
+    jQuery(".media-menu").removeClass("visible");
     return false;
   });
 
   var current = "product-settings";
-  jQuery('.media-modal-content', $popup).attr('data-mode', current);
-  jQuery('.media-modal-content', $popup).attr('data-active-dialog', current);
-  jQuery('.media-menu-item')
-      .removeClass('active')
-      .filter('[data-content=' + current + ']').addClass('active');
+  jQuery(".media-modal-content", $popup).attr("data-mode", current);
+  jQuery(".media-modal-content", $popup).attr("data-active-dialog", current);
+  jQuery(".media-menu-item")
+      .removeClass("active")
+      .filter("[data-content=" + current + "]").addClass("active");
   
   function epages_open_popup() {
-    $popup.addClass('open');
+    $popup.addClass("open");
   }
 });
