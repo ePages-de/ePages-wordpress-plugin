363a062dec0b57e714c4f9001edc23b1990ee5e7
dignatieva <d.ignatieva@epages.com>
2016-03-24 12:10:17 +0100

    Options page style changes

--- templates/options_page.php
+++ templates/options_page.php
@@ -1,25 +1,26 @@
 <div class="wrap">
-
-
 <?php if (get_option("epages_api_url_confirmed"  , 'epages')) { ?>
   <div class="wrap">
     <div class="epages-verified-shop-text"><i class="fa fa-check-circle fa-4x"></i><span><b>
       <?php _e('Congratulations! Your Online Shop is now connected with your Website!' , 'epages'); ?></b></span></div>
   </div>
   <br>
-  <p><?php _e('Now you can add your Online Shop to any Wordpress page or post. Just click the Add Online Shop button.'  , 'epages'); ?></p>
-  <p><?php _e('After that click Edit to edit product and appearance settings for Online Shop for this page or post.'  , 'epages'); ?></p>
-  <p><?php _e('Have a question? Visit our <a href="https://www.online-help-center.com/customer/en/portal/articles?utm_source=ePages&utm_medium=Menu%20Entry&utm_campaign=website">help center'  , 'epages'); ?></a></p>
-  <p><?php _e('Your API URL is: ' , 'epages'); ?><b><?php echo get_option("epages_api_url" , 'epages') ?></b></p>
+  <p><?php _e('Now you can add your Online Shop to any Wordpress page or post. Just click the <strong>Add Online Shop</strong> button.'  , 'epages'); ?><br>
+  <?php _e('After that click <strong>Edit</strong> to edit product and appearance settings for Online Shop for this page or post.'  , 'epages'); ?></p>
+  <p><strong><?php _e('Have a question? Visit our <a href="https://www.online-help-center.com/customer/en/portal/articles?utm_source=ePages&utm_medium=Menu%20Entry&utm_campaign=website">help center'  , 'epages'); ?></a></strong></p>
 
-  <a href="<?php echo get_option("epages_mbo_url") ?>"> <?php submit_button( __('Go to shop administration' , 'epages')) ?></a>
-
+  <div id="setting-error-tgmpa" class="updated settings-error notice">
+  <h4><strong> <?php _e('Your API URL is: ' , 'epages'); ?> <em><?php echo get_option("epages_api_url" , 'epages') ?></b></em></strong></h4>
+  <p><strong><a target="_blank" href="<?php echo get_option("epages_mbo_url") ?>">Go to shop administration</a>
+    | <a class="dismiss-notice" href="/wp-admin/edit.php?post_type=page" target="_parent">Add shop to the page</a></strong></p><p></p><button type="button" class="notice-dismiss"><span class="screen-reader-text">Dismiss this notice.</span></button></div>
+  <div class="md-8 pull-right">
   <form method="post" action="options.php">
     <?php settings_fields("epages_options_page"); ?>
     <input type="hidden" name="epages_api_url" value="">
     <input type="hidden" name="epages_api_url_confirmed" value="0">
     <?php submit_button( __('Disconnect Online Shop' , 'epages')) ?>
   </form>
+</div></div>
 <?php } else { ?>
   <h2><?php _e('Connect your Online Shop with the Plugin'  , 'epages'); ?></h2>
   <p><b><?php _e('There are just a few steps left to start selling on your Wordpress site' , 'epages'); ?></b></p>
@@ -31,6 +32,7 @@
     <li><?php _e('Enter your API url here. ','epages'); ?><a href="https://www.online-help-center.com/customer/en/portal/articles?utm_source=ePages&utm_medium=Menu%20Entry&utm_campaign=website"><?php _e('Learn more' , 'epages'); ?></a> <?php _e('how to attain the URL' , 'epages'); ?></li>
   </ol>
 
+
   <form method="post" action="options.php">
     <?php settings_fields("epages_options_page"); ?>
     <input type="text" name="epages_api_url" size=60
@@ -40,5 +42,5 @@
       <span class="epages-shop-form-failure"><?php _e('Invalid shop URL' , 'epages'); ?></span>
     <?php } submit_button( __('Save' , 'epages')) ?>
   </form>
-</div>
+
 <?php } ?>
--- templates/popup.php
+++ templates/popup.php
@@ -65,8 +65,11 @@
             <?php _e('Show product sorting' ,'epages'); ?>
             <span class="tooltip" data-tooltip="Mark this checkbox, if you want to display a product sorting option in your product list view."><i class="fa fa-lg fa-info-circle"></i></span>
           </label>
-        </div>
 
+          <div>
+            <p><?php _e('Additionally, you can add a basket widget to your website using'  , 'epages'); ?> <a target="_blank" href="widgets.php"><?php _e('WordPress native widgets'  , 'epages'); ?></a>     </p>
+          </div>
+        </div>
         <div class="media-frame-toolbar">
           <div class="media-toolbar">
             <div class="media-toolbar-secondary">
